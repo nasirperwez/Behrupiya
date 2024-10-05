60bb846ef147cc370738accffac6a7ac46ef4976
nasir perwez <nasir.perwez@gmail.com>
2024-10-05 21:52:01 +0530

    Imgae generation complet

--- app/src/main/java/com/eramlab/behrupiya/presentation/ui/screens/generate/GenerateImageScreen.kt
+++ app/src/main/java/com/eramlab/behrupiya/presentation/ui/screens/generate/GenerateImageScreen.kt
@@ -2,15 +2,22 @@ package com.eramlab.behrupiya.presentation.ui.screens.generate
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.eramlab.behrupiya.presentation.SharedViewModel
 import com.eramlab.behrupiya.presentation.ui.screens.generate.component.GenerateCard
@@ -28,7 +35,7 @@ fun GenerateImageScreen(
     val categories by generateImageViewModel.categories.collectAsState()
     val selectedCategory by generateImageViewModel.selectedCategory.collectAsState()
     val items by generateImageViewModel.items.collectAsState()
-   // val isLoading by generateImageViewModel.isLoading.collectAsState()
+    val isLoading by generateImageViewModel.isLoading.collectAsState()
 
     Column(
         modifier = Modifier
@@ -49,20 +56,53 @@ fun GenerateImageScreen(
                 .weight(0.75f),
 
         )
-        GenerateCard(
-            sharedViewModel,
-            items = items,
-            categories = categories,
-            selectedCategory = selectedCategory,
-            onCategorySelected = {  },
-            modifier = Modifier
+//        if(isLoading) {
+//            GenerateCard(
+//                sharedViewModel,
+//                items = items,
+//                categories = categories,
+//                selectedCategory = selectedCategory,
+//                onCategorySelected = { },
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .fillMaxHeight()
+//                    .weight(0.25f)
+//
+//
+//            )
+//        }
+//        else{
+            LoadingWrapper(
+                isLoading = isLoading,
+                loadingMessage = "Generating image...",
+                modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
                 .weight(0.25f)
+            )
+//        }
+    }
+}
 
 
-        )
 
+@Composable
+fun LoadingWrapper(
+    isLoading: Boolean,
+    loadingMessage: String = "Loading...",
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (isLoading) {
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+                Spacer(modifier = modifier.height(16.dp))
+                Text(text = loadingMessage)
+            }
+        }
     }
 }
 
