7a2144aae198bc08082efe8a03fb8a184a7e70d9
nasir perwez <nasir.perwez@gmail.com>
2024-10-07 17:56:14 +0530

    remived key

--- app/src/main/java/com/eramlab/behrupiya/utils/AppConstants.kt
+++ app/src/main/java/com/eramlab/behrupiya/utils/AppConstants.kt
@@ -10,8 +10,6 @@ object AppConstants {
     const val GENERATE_IMAGE_ENDPOINT = "$BASE_URL/generate-image/"
 
 
-
-
 //    const val BASE_URL = "http://192.168.1.3:8080"
 //    const val IMG_BASE_ENDPOINT = "$BASE_URL/static/"
 //
