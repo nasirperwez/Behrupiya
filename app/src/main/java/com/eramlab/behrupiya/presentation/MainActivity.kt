fdd2b42ffaa5e5e1ee93b39bed2c700acbdc0524
abhimanyukumarsingh123 <akturki11122004@gmail.com>
2024-10-16 18:32:33 +0530

    History Screen complete

--- app/src/main/java/com/eramlab/behrupiya/presentation/MainActivity.kt
+++ app/src/main/java/com/eramlab/behrupiya/presentation/MainActivity.kt
@@ -14,8 +14,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                ControlNav()
-                //HistoryScreen()
+               // ControlNav()
+                HistoryScreen()
             }
         }
     }
--- app/src/main/java/com/eramlab/behrupiya/presentation/ui/screens/History/HistoryScreen.kt
+++ app/src/main/java/com/eramlab/behrupiya/presentation/ui/screens/History/HistoryScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.eramlab.behrupiya.presentation.ui.screens.History.Compnent.HistoryImage
 import com.eramlab.behrupiya.presentation.ui.screens.History.Compnent.HistoryTopBar
+import com.eramlab.behrupiya.presentation.ui.screens.History.Component.HistoryImage
 
 @Composable
 fun HistoryScreen() {
